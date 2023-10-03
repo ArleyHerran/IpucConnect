875b2743268613db19319676ca9714edae7b53cb
unknown <superjuan515@gmail.com>
2023-10-03 16:10:30 -0500

    Agregacion de campos form

--- src/components/FormMiembros.vue
+++ src/components/FormMiembros.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- Diálogo de Registro/Edit -->
-  <v-dialog v-model="estados.formMiembros.display" max-width="600" :persistent="true">
+  <v-dialog
+    v-model="estados.formMiembros.display"
+    max-width="600"
+    :persistent="true"
+  >
     <v-card>
       <v-card-title class="dialog-title">
         {{
@@ -12,10 +16,8 @@
             ? "Vista"
             : "Texto por defecto"
         }}
-
       </v-card-title>
 
-     
       <v-card-text>
         <v-form ref="form" @submit="saveMember">
           <!-- Campos del formulario -->
@@ -33,7 +35,7 @@
 
           <!-- Número de documento -->
           <v-text-field
-          :disabled="estados.formMiembros.mode == 'edit'"
+            :disabled="estados.formMiembros.mode == 'edit'"
             v-model="formData.numeroDocumento"
             label="Número de documento"
             type="number"
@@ -84,6 +86,31 @@
             v-model="formData.direccion"
             label="Dirección"
           ></v-text-field>
+          <v-select
+            v-model="formData.nivelAcademico"
+            :items="nivelesAcademicos"
+            label="Nivel académico"
+          ></v-select>
+          <v-radio-group
+            v-model="formData.discapacitado"
+            label="¿Tiene usted alguna discapacidad?"
+          >
+            <v-radio label="Sí" value="Sí"></v-radio>
+            <v-radio label="No" value="No"></v-radio>
+          </v-radio-group>
+
+          <v-select
+            v-if="formData.discapacitado === 'Sí'"
+            v-model="formData.discapacidad"
+            :items="tiposDiscapacidad"
+            label="Tipo de discapacidad"
+          ></v-select>
+
+          <v-text-field
+          v-if="formData.discapacidad === 'Otra'"
+            v-model="formData.descripcionDiscapacidad"
+            label="Descripcion de Discapacidad"
+          ></v-text-field>
 
           <!-- Sexo -->
           <v-radio-group v-model="formData.sexo" label="Sexo">
@@ -94,7 +121,7 @@
           <!-- Estado Civil -->
           <v-select
             v-model="formData.estadoCivil"
-            :items="['Casado/a', 'Soltero/a']"
+            :items="['Casado/a', 'Soltero/a', 'Viudo/a', 'Divorciado/a']"
             label="Estado Civil"
           ></v-select>
 
@@ -122,15 +149,48 @@
             v-model="formData.nombrePastorBautismo"
             label="Nombre del pastor que lo bautizó"
           ></v-text-field>
+          <div>
+            <v-text-field v-model="formData.cargo" label="Cargo" required>
+              <template #append>
+                <v-btn
+                  :loading="isLoading"
+                  class="flex-grow-1"
+                  height="48"
+                  variant="tonal"
+                  @click="agregarCargo"
+                >
+                  <v-icon icon="mdi-check"></v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
+
+            <v-chip-group filter>
+              <v-chip
+                v-for="(pastor, index) in pastorList"
+                :key="index"
+                :color="index === editedIndex ? editedColor : ''"
+              >
+                {{ pastor }}
+                <v-icon @click="eliminarPastor(index)">mdi-close</v-icon>
+              </v-chip>
+            </v-chip-group>
+          </div>
+          <v-radio-group
+            v-model="formData.espiritu"
+            label="¿Es usted sellado/a con el Espiritu Santo?"
+          >
+            <v-radio label="Sí" value="Sí"></v-radio>
+            <v-radio label="No" value="No"></v-radio>
+          </v-radio-group>
           <v-textarea
             v-model="formData.referenciaPastoral"
             label="Referencia Pastoral"
           ></v-textarea>
         </v-form>
       </v-card-text>
-     
+
       <v-card-actions class="dialog-actions">
-        <v-spacer/>
+        <v-spacer />
         <v-btn @click="estados.formMiembros.display = false">
           {{
             estados.formMiembros.mode === "view" ? "Cerrar" : "Cancelar"
@@ -152,23 +212,31 @@
 <script setup>
 import { ref, reactive, watch, onMounted } from "vue";
 import { useAppStore } from "../store/app";
-import { collection, addDoc, doc, updateDoc,query,getDocs,where,serverTimestamp} from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  query,
+  getDocs,
+  where,
+  serverTimestamp,
+} from "firebase/firestore";
 import { auth, db } from "../ConfigFirebase";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 const estados = useAppStore();
 
 const dialog = ref(false);
 const editMode = ref(false);
 
 const dataDefault = reactive({
-  sede:{},
+  sede: {},
   datosRegistro: {
     timestamp: serverTimestamp(),
-    sede: {}
+    sede: {},
   },
   historiaTraslados: [],
 });
-
 
 const formData = reactive({
   tipoDocumento: "",
@@ -176,17 +244,40 @@ const formData = reactive({
   nombre: "",
   apellido: "",
   rol: "",
-  fechaNacimiento: null,
+  fechaNacimiento: "",
   celular: "",
   direccion: "",
+  nivelAcademico: "",
+  discapacitado:"",
+  discapacidad:"",
+  descripcionDiscapacidad:"",
   sexo: "",
   estadoCivil: "",
   esBautizado: "",
-  fechaBautismo: null,
+  cargo: [],
+  fechaBautismo: "",
   nombrePastorBautismo: "",
   referenciaPastoral: "",
 });
 
+const nivelesAcademicos = [
+  "Educación No aplica",
+  "Educación Básica Primaria",
+  "Educación Básica Secundaria",
+  "Educación Media o Bachillerato",
+  "Educación Técnica",
+  "Educación Tecnológica",
+  "Formación Profesional",
+  "Sin Educación (No sabe leer)",
+];
+
+const tiposDiscapacidad = [
+  "Física",
+  "Visual",
+  "Auditiva",
+  "Intelectual",
+  "Otra",
+];
 // Usamos watch para observar cambios en estados.data y actualizar formData
 watch(
   () => estados.data,
@@ -211,17 +302,16 @@ watch(
           formData[prop] = objet1[prop];
         }
       }
-    }else{
+    } else {
       clearFormFields();
-
     }
   }
 );
 const actualizarFormData = (newData) => {
   if (newData) {
-    dataDefault.sede=newData;
+    dataDefault.sede = newData;
     dataDefault.datosRegistro.sede = newData;
-  
+
     // También puedes actualizar otras propiedades aquí si es necesario
   }
 };
@@ -236,10 +326,10 @@ const saveMember = async () => {
   const miembro = { ...dataDefault, ...formData };
   //console.log(JSON.parse(JSON.stringify(miembro)));
   if (!validateForm(miembro)) return;
- 
+
   if (estados.formMiembros.mode === "add") {
     const resultado = await buscarV(miembro.numeroDocumento);
-    if(resultado)return;
+    if (resultado) return;
     guardarRegistro(miembro);
   } else {
     editar();
@@ -256,8 +346,12 @@ async function guardarRegistro(d) {
     swal("Éxito!", "El registro se ha guardado correctamente.", "success");
   } catch (error) {
     // Muestra un mensaje de error si hubo un problema al guardar
-   
-    swal("Error!", "Hubo un problema al guardar el registro en Firebase."+error, "error");
+
+    swal(
+      "Error!",
+      "Hubo un problema al guardar el registro en Firebase." + error,
+      "error"
+    );
   }
 }
 //Permite guardar modificacion de cualquier miembro
@@ -271,29 +365,27 @@ async function editar() {
     swal("Éxito!", "El registro se ha actualizado correctamente.", "success");
   } catch (error) {
     // Muestra un mensaje de error si hubo un problema al actualizar
-    
-    swal("Error!", "Hubo un problema al actualizar el registro en Firebase.", "error");
+
+    swal(
+      "Error!",
+      "Hubo un problema al actualizar el registro en Firebase.",
+      "error"
+    );
   }
 }
-
 
 //FUNCIONES
 
 function clearFormFields() {
-  formData.tipoDocumento = "";
-  formData.numeroDocumento = "";
-  formData.nombre = "";
-  formData.apellido = "";
-  formData.rol = "";
-  formData.fechaNacimiento = null;
-  formData.celular = "";
-  formData.direccion = "";
-  formData.sexo = "";
-  formData.estadoCivil = "";
-  formData.esBautizado = "";
-  formData.fechaBautismo = null;
-  formData.nombrePastorBautismo = "";
-  formData.referenciaPastoral = "";
+  for (const key in formData) {
+    if (Array.isArray(formData[key])) {
+      formData[key] = [];
+    } else if (typeof formData[key] === 'string') {
+      formData[key] = '';
+    } else if (typeof formData[key] === 'number') {
+      formData[key] = null;
+    }
+  }
 }
 
 function validateForm(d) {
@@ -307,24 +399,38 @@ function validateForm(d) {
     return false;
   };
 
-  if (!d.sede.id || !d.sede.distrito || !d.sede.zona || !d.sede.municipio || !d.sede.distrito) {
+  if (
+    !d.sede.id ||
+    !d.sede.distrito ||
+    !d.sede.zona ||
+    !d.sede.municipio ||
+    !d.sede.distrito
+  ) {
     return showError("Error desconocido.");
   }
 
   if (!d.tipoDocumento || d.tipoDocumento.length > 30) {
-    return showWarning("El campo Tipo de documento no puede quedar vacío o tener más de 30 caracteres.");
+    return showWarning(
+      "El campo Tipo de documento no puede quedar vacío o tener más de 30 caracteres."
+    );
   }
 
   if (!d.numeroDocumento || d.numeroDocumento.length > 30) {
-    return showWarning("El campo número de documento no puede quedar vacío o tener más de 30 caracteres.");
+    return showWarning(
+      "El campo número de documento no puede quedar vacío o tener más de 30 caracteres."
+    );
   }
 
   if (!d.nombre || d.nombre.length > 40) {
-    return showWarning("El campo nombre no puede quedar vacío o tener más de 40 caracteres.");
+    return showWarning(
+      "El campo nombre no puede quedar vacío o tener más de 40 caracteres."
+    );
   }
 
   if (!d.apellido || d.apellido.length > 40) {
-    return showWarning("El campo apellido no puede quedar vacío o tener más de 40 caracteres.");
+    return showWarning(
+      "El campo apellido no puede quedar vacío o tener más de 40 caracteres."
+    );
   }
 
   if (!d.rol || d.rol.length > 40) {
@@ -336,11 +442,42 @@ function validateForm(d) {
   }
 
   if (!d.celular || d.celular.length > 16) {
-    return showWarning("El campo celular no puede quedar vacío o tener más de 16 caracteres.");
+    return showWarning(
+      "El campo celular no puede quedar vacío o tener más de 16 caracteres."
+    );
   }
 
   if (!d.direccion || d.direccion.length > 60) {
-    return showWarning("El campo dirección no puede quedar vacío o tener más de 60 caracteres.");
+    return showWarning(
+      "El campo dirección no puede quedar vacío o tener más de 60 caracteres."
+    );
+  }
+  if (!d.nivelAcademico) {
+    return showWarning(
+      "El campo nivel academico debe ser completado"
+    );
+  }
+
+  if (!d.discapacitado) {
+    return showWarning(
+      "El campo ¿Tiene usted alguna discapacidad? debe ser completado"
+    );
+  }
+
+  if (d.discapacitado === "Sí") {
+    if (!d.discapacidad) {
+      return showWarning("El campo Tipo de discapacidad no puede quedar vacío.");
+    }
+
+    if (d.discapacidad === "Otra") {
+      if (!d.descripcionDiscapacidad || d.descripcionDiscapacidad.length > 100) {
+      return showWarning(
+        "El campo Descripcion de discapacidad no puede quedar vacío o tener más de 10 caracteres."
+      );
+    }
+    }
+  } else {
+  
   }
 
   if (!d.sexo) {
@@ -361,28 +498,44 @@ function validateForm(d) {
     }
 
     if (!d.nombrePastorBautismo || d.nombrePastorBautismo.length > 50) {
-      return showWarning("El campo nombre del pastor que le bautizó no puede quedar vacío o tener más de 50 caracteres.");
+      return showWarning(
+        "El campo nombre del pastor que le bautizó no puede quedar vacío o tener más de 50 caracteres."
+      );
     }
-  }else{
-    d.fechaBautismo=""
-    d.nombrePastorBautismo=""
+  } else {
+    d.fechaBautismo = "";
+    d.nombrePastorBautismo = "";
+  }
+
+
+  if (!d.espiritu) {
+    return showWarning("El campo ¿Es usted sellado/a con el Espiritu Santo? no puede quedar vacío.");
   }
 
   return true;
 }
 
 watch(
-  () => formData.esBautizado,
+  () => formData,
   (newData) => {
+   
 
-    if (newData=="No") {
-      formData.fechaBautismo=""
-      formData.nombrePastorBautismo=""
-    } 
-    
-  }
+    if (newData.discapacitado === "No") {
+      formData.discapacidad = "";
+      formData.descripcionDiscapacidad = "";
+    }
+
+    if (newData.discapacidad !== "Otra") {
+      formData.descripcionDiscapacidad = "";
+    }
+
+    if (newData.esBautizado === "No") {
+      formData.fechaBautismo = null;
+      formData.nombrePastorBautismo = "";
+    }
+  },
+  { deep: true } // Utiliza el option 'deep' para rastrear cambios en propiedades internas
 );
-
 async function buscarV(n) {
   const q = query(
     collection(db, "Membresia"),
@@ -393,14 +546,47 @@ async function buscarV(n) {
   if (querySnapshot.empty) {
     return false; // No se encontraron documentos
   } else {
-    
-    swal("Error!", "Ya existe un registro con este número de documento!", "error");
-   
+    swal(
+      "Error!",
+      "Ya existe un registro con este número de documento!",
+      "error"
+    );
 
     return true; // Se encontró al menos un documento
   }
 }
 
+const pastorList = ref([]);
+const editedIndex = ref(-1); // Variable para controlar el índice editado
+const editedColor = "green"; // Color para la nueva chip
+
+/*
+const agregarPastor = () => {
+  if (formData.cargo.trim() !== "") {
+    pastorList.value.push(formData.cargo);
+    formData.cargo = ""; // Limpiar el campo después de agregar
+  }
+};*/
+
+const isLoading = ref(false);
+const agregarCargo = () => {
+  isLoading.value = true; // Activar la carga al hacer clic en el icono
+
+  setTimeout(() => {
+    if (formData.cargo.trim() !== "") {
+      const nuevoPastor = formData.cargo;
+      pastorList.value.unshift(nuevoPastor); // Agregar al principio del array
+      formData.cargo = ""; // Limpiar el campo después de agregar
+      editedIndex.value = 0; // Establecer el índice editado como 0 para que sea verde
+    }
+
+    isLoading.value = false;
+  }, 1000); // 2 segundos
+};
+
+const eliminarPastor = (index) => {
+  pastorList.value.splice(index, 1);
+};
 </script>
 
 <style scoped>
@@ -427,7 +613,6 @@ async function buscarV(n) {
   padding: 6px 8px; /* Espaciado igual a otros campos */
 }
 
-
 .dialog-title {
   position: sticky;
   top: 0;
@@ -435,14 +620,12 @@ async function buscarV(n) {
   z-index: 1; /* Asegura que los botones estén por encima del contenido */
 }
 
-
 .dialog-actions {
   position: sticky;
   bottom: 0;
   background-color: white;
   z-index: 1; /* Asegura que los botones estén por encima del contenido */
 }
-
 
 .dialog-button {
   margin-right: 16px; /* Espaciado entre los botones */
